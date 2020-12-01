c7d6f00e3dd1c0b41a66e1fc980ea88e56e365a4
raphaelcamar <raphaelcamar@outlook.com>
2020-12-01 13:32:02 -0300

    fixing errors

---
+++ server.js
@@ -0,0 +1,11 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 8080;
+server.use(middlewares);
+server.use(router);
+server.listen(port, () =>{
+    console.lof(`JSON Server is running in ${port}`)
+})
